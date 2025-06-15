5466bd604fd281d02c21edef8eaae709cb420f27
rubenstvares <rubensgabrieltavaresferreira@gmail.com>
2025-06-15 20:20:46 -0300

    Conexao.java

--- CadastroDigitalSpa/src/main/java/br/com/faseh/cadastroclientes/util/Conexao.java
+++ CadastroDigitalSpa/src/main/java/br/com/faseh/cadastroclientes/util/Conexao.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String URL = "jdbc:mysql://localhost:3306/sistema_clientes";
+    private static final String URL = "jdbc:mysql://localhost:3306/sistema_clientes?allowPublicKeyRetrieval=TRUE";
     private static final String USER = "root";
     private static final String PASSWORD = "123456"; // <- Altere conforme sua senha
 
